610a3f2a151f3a047fbcbb771e90c4aebc0a6a8d
Nermirc <141329241+Nermirc@users.noreply.github.com>
2023-11-01 13:37:27 +0300

    Serial Value Calculation
    contiuning to calculating using do while loops

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   int i,n;
+   float x,seri;
+   char devam;
+
+   do
+   {
+       printf("Pozitif sayi giriniz.\n");
+   scanf("%d",&n);
+   printf("Reel sayi :");
+   scanf("%f %c",&x,&devam);
+
+   for(i=1;i<=2*n-1;i+=2)
+   {
+       seri+=i/pow(x,i+1);
+   }
+   printf("Seri = %f\n",seri);
+
+
+   }while((devam=='E')|| (devam =='e'));
+   printf("Hoscakalin");
+
+
+}
